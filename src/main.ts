1cbb369cd4a0cfe5b95c12cd807176c3baf84105
Alexnander Nasokin <alexnas2python@gmail.com>
2024-09-07 19:14:35 +0300

    clean app

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import './assets/main.css'
+import './assets/styles/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +10,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(VueQueryPlugin)
 app.use(router)
 
 app.mount('#app')
